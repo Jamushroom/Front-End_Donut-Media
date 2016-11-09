c96bbede2fd7e5964be48cf7b6cdf35e4aa97ef5
Patrick Attema <patrickattema@home.nl>
2016-11-09 12:27:44 +0100

    v0.1

---
+++ header.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<div id="Header">
+
+<img src="img/icon_voorwaarden.png" alt="" width="35" height="35" id="icon_voorwrdn"/>
+<img src="img/icon_info.png" alt="" width="35" height="35" id="icon_info"/>
+<img src="img/icon_logout.png" alt="" width="35" height="35" id="icon_logout"/>
+
+</div>
