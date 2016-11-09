9c42ea26b5aa2d7ecdeda787a633ac973c496a04
Patrick Attema <patrickattema@home.nl>
2016-11-09 12:34:06 +0100

    v1.0.1

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <link href="css/style.css" rel="stylesheet" type="text/css">
 <div id="Header">
 
-<img src="img/icon_voorwaarden.png" alt="" width="35" height="35" id="icon_voorwrdn"/>
+<img src="img/icon_disclaimer.png" alt="" width="35" height="35" id="icon_disclaimer"/>
 <img src="img/icon_info.png" alt="" width="35" height="35" id="icon_info"/>
 <img src="img/icon_logout.png" alt="" width="35" height="35" id="icon_logout"/>
 
