285b8a08c750606ef7b9ef8a76f38c98fa106c17
RenzeD98 <renze.d98@gmail.com>
2016-11-10 09:37:24 +0100

    2nd Version Landings Page
    Custom Facebook button
    If checkbox terms and services is checked the Facebookbutton is enabled
    Landingspage

--- index.php
+++ index.php
@@ -5,9 +5,42 @@
   <title></title>
   <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Hind" rel="stylesheet">
 </head>
 <body>
+<!-- include Facebook JS-SDK -->
+<!-- <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/nl_NL/sdk.js#xfbml=1&version=v2.8&appId=1226960660659176";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script> -->
 
+  <h1 class="indexTitle">Welkom bij De stem van Leeuwarden</h1>
+
+  <div class="fb-login-wrapper">
+    <a href="#">
+      <input type="button" disabled="disabled" class="fb-login-button" id="login" value="Aanmelden met Facebook   ">
+    </a>
+  </div>
+
+  <div class="termsAndConditionsWrapper">
+    <input type="checkbox" id="conditionsCheckbox">
+    <p>Ik ga akkoord met de
+    <a href="#" id="termsAndConditions">Algemene Voorwaarden.</a></p>
+  </div>
+
+    <script type='text/javascript'>
+          document.getElementById('conditionsCheckbox').onclick=function(){
+              if(this.checked == false){
+                   document.getElementById('login').disabled = true;
+              } else {
+                  document.getElementById('login').removeAttribute('disabled');
+              }
+          };
+  </script>
 
 </body>
 </html>
