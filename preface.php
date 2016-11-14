8d61b98d9cc608c2f2ce8acd1774d77c82ca1022
RenzeD98 <renze.d98@gmail.com>
2016-11-14 12:31:43 +0100

    Made the Preface.php and added css for the preface

---
+++ preface.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="nl">
+<head>
+  <meta charset=utf-8>
+  <title></title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Hind" rel="stylesheet">
+</head>
+<body>
+  <div class="mainWrapper">
+    <h1 class="subPageTitle">Stem van Leeuwarden</h1>
+
+    <article id="prefaceP">Beste gebruiker. Je staat op het punt deze enquête in te vullen voor de smaak van leeuwarden. Een lopend project binnen de NHL. Door op de knop "Start Enquête" te drukken begin je met de enquête. Er zullen kort een paar vragen gesteld worden over je favoriete restaurants in Leeuwarden. Het invullen kost slechts enkele minuten.
+    <h5>Succes!</h5>
+    </article>
+    <div class="buttonCenter">
+      <a href="#">
+        <input type="button" id="startE" class="button gradientcolor" title="button"
+        value="Start Enquête">
+      </a>
+    </div>
+  </div>
+</body>
+</html>
