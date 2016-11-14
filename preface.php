00aecf0963c3032afd8c035d80a3ba0e5e5e8bc2
RenzeD98 <renze.d98@gmail.com>
2016-11-14 12:34:43 +0100

    Changed the spacing in the article so the html layout is correct

--- preface.php
+++ preface.php
@@ -11,8 +11,9 @@
   <div class="mainWrapper">
     <h1 class="subPageTitle">Stem van Leeuwarden</h1>
 
-    <article id="prefaceP">Beste gebruiker. Je staat op het punt deze enquête in te vullen voor de smaak van leeuwarden. Een lopend project binnen de NHL. Door op de knop "Start Enquête" te drukken begin je met de enquête. Er zullen kort een paar vragen gesteld worden over je favoriete restaurants in Leeuwarden. Het invullen kost slechts enkele minuten.
-    <h5>Succes!</h5>
+    <article id="prefaceP">
+      Beste gebruiker. Je staat op het punt deze enquête in te vullen voor de smaak van leeuwarden. Een lopend project binnen de NHL. Door op de knop "Start Enquête" te drukken begin je met de enquête. Er zullen kort een paar vragen gesteld worden over je favoriete restaurants in Leeuwarden. Het invullen kost slechts enkele minuten.
+      <h5>Succes!</h5>
     </article>
     <div class="buttonCenter">
       <a href="#">
