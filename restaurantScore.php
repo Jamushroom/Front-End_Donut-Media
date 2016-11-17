e7702c21ab158b09f77246d23da3f88ac0bc1422
RenzeD98 <renze.d98@gmail.com>
2016-11-17 13:26:01 +0100

    Made page and added css for the basic of the enquete

---
+++ restaurantScore.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="nl">
+<head>
+  <meta charset=utf-8>
+  <title></title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Hind" rel="stylesheet">
+</head>
+<body>
+  <div class="mainWrapper">
+    <h1 class="subPageTitle">Stem van Leeuwarden</h1>
+    </section>
+    <div class="buttonCenter">
+      <a href="#">
+        <input type="button" class="button gradientcolor" title="button"
+        value="Volgende vraag">
+      </a>
+    </div>
+  </div>
+</body>
+</html>
+
