bd3fdb6947cb99c6215a6cc0ab67c25dd46d3532
RenzeD98 <renze.d98@gmail.com>
2016-11-21 14:44:44 +0100

    restaurantScore html & css done

--- restaurantScore.php
+++ restaurantScore.php
@@ -14,24 +14,72 @@
     <!-- Restaurant # --></h3>
     <section id="RestaurantScoreSection">
 
-      <div>
-        <input type="checkbox" id="checkbox-1-1" class="regular-checkbox" onclick="disable()">
-        <label class="checkbox-1-2" for="checkbox-1-1" style="top: 5px"></label>
-        <label class="question" for="checkbox-1-1">Goede kwaliteit van het eten
+      <!-- slider 1 -->
+      <div class="sliderContainer">
+        <input type="checkbox" id="checkbox-1" class="regular-checkbox" onclick="disable()">
+        <label for="checkbox-1" style="top: 5px"></label>
+        <label class="question" for="checkbox-1">Goede kwaliteit van het eten
         </label>
 
-        <input id="slider" type="range" min="1" max="5" step="1" value="3" disabled="disabled">
+        <input id="slider-1" type="range" min="1" max="11" step="1" value="6" disabled="disabled">
+
+        <p class="sliderText neutraalT">Neutraal</p>
+        <p class="sliderText goedT">Goed</p>
+        <p class="sliderText uitstekendT">Uitstekend</p>
       </div>
 
-      <script>
-        function disable(){
-          if (document.getElementById('checkbox-1-1').checked == false) {
-            document.getElementById('slider').setAttribute("disabled", "disabled");
-            } else {
-              document.getElementById('slider').removeAttribute('disabled');
-            }
-          }
-      </script>
+
+      <!-- slider 2 -->
+      <div class="sliderContainer">
+        <input type="checkbox" id="checkbox-2" class="regular-checkbox" onclick="disable()">
+        <label for="checkbox-2" style="top: 5px"></label>
+        <label class="question" for="checkbox-2">Goede prijs-kwaliteitverhouding
+        </label>
+
+        <input id="slider-2" type="range" min="1" max="11" step="1" value="6" disabled="disabled">
+        <p class="sliderText neutraalT">Neutraal</p>
+        <p class="sliderText goedT">Goed</p>
+        <p class="sliderText uitstekendT">Uitstekend</p>
+      </div>
+
+      <!-- slider 3-->
+      <div class="sliderContainer">
+        <input type="checkbox" id="checkbox-3" class="regular-checkbox" onclick="disable()">
+        <label for="checkbox-3" style="top: 5px"></label>
+        <label class="question" for="checkbox-3">Voldoende variatie in het menu
+        </label>
+
+        <input id="slider-3" type="range" min="1" max="11" step="1" value="6" disabled="disabled">
+        <p class="sliderText neutraalT">Neutraal</p>
+        <p class="sliderText goedT">Goed</p>
+        <p class="sliderText uitstekendT">Uitstekend</p>
+      </div>
+
+      <!-- slider 4 -->
+      <div class="sliderContainer">
+        <input type="checkbox" id="checkbox-4" class="regular-checkbox" onclick="disable()">
+        <label for="checkbox-4" style="top: 5px"></label>
+        <label class="question" for="checkbox-4">Aangename sfeer in het restaurant
+        </label>
+
+        <input id="slider-4" type="range" min="1" max="11" step="1" value="6" disabled="disabled">
+        <p class="sliderText neutraalT">Neutraal</p>
+        <p class="sliderText goedT">Goed</p>
+        <p class="sliderText uitstekendT">Uitstekend</p>
+      </div>
+
+      <!-- slideer 5 -->
+      <div class="sliderContainer">
+        <input type="checkbox" id="checkbox-5" class="regular-checkbox" onclick="disable()">
+        <label for="checkbox-5" style="top: 5px"></label>
+        <label class="question" for="checkbox-5">Snelle/vriendelijke bediening
+        </label>
+
+        <input id="slider-5" type="range" min="1" max="11" step="1" value="6" disabled="disabled">
+        <p class="sliderText neutraalT">Neutraal</p>
+        <p class="sliderText goedT">Goed</p>
+        <p class="sliderText uitstekendT">Uitstekend</p>
+      </div>
 
     </section>
     <div class="buttonCenter">
@@ -41,6 +89,42 @@
       </a>
     </div>
   </div>
+
+<!-- js -->
+      <script>
+        function disable(){
+          if (document.getElementById('checkbox-1').checked == false) {
+            document.getElementById('slider-1').setAttribute("disabled", "disabled");
+            } else {
+              document.getElementById('slider-1').removeAttribute('disabled');
+            }
+
+          if (document.getElementById('checkbox-2').checked == false) {
+            document.getElementById('slider-2').setAttribute("disabled", "disabled");
+            } else {
+              document.getElementById('slider-2').removeAttribute('disabled');
+            }
+
+          if (document.getElementById('checkbox-3').checked == false) {
+            document.getElementById('slider-3').setAttribute("disabled", "disabled");
+            } else {
+              document.getElementById('slider-3').removeAttribute('disabled');
+            }
+
+          if (document.getElementById('checkbox-4').checked == false) {
+            document.getElementById('slider-4').setAttribute("disabled", "disabled");
+            } else {
+              document.getElementById('slider-4').removeAttribute('disabled');
+            }
+
+          if (document.getElementById('checkbox-5').checked == false) {
+            document.getElementById('slider-5').setAttribute("disabled", "disabled");
+            } else {
+              document.getElementById('slider-5').removeAttribute('disabled');
+            }
+
+          }
+      </script>
 </body>
 </html>
 
