803652b51e24aeb0cd6ab4b0214c3754997221a3
RenzeD98 <renze.d98@gmail.com>
2016-11-22 10:18:17 +0100

    added the page "endpage"

---
+++ EndPage.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="nl">
+<head>
+  <meta charset=utf-8>
+  <title></title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Hind" rel="stylesheet">
+</head>
+<body>
+  <div class="mainWrapper">
+    <h1 class="subPageTitle">Stem van Leeuwarden</h1>
+    <h3 id="endTitle">Bedankt voor het invullen van de enquête!</h3>
+  </div>
+</body>
+</html>
