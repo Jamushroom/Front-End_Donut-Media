770c9d4678e8a0c0aa1a69e35a1be8d9a2ea0c28
Jamushroom <jamushmail@gmail.com>
2016-11-22 14:28:36 +0100

    Merge branch 'master' into Dialog_size
    # Conflicts:
    #	css/style.css

---
+++ EndPage.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="nl">
+<head>
+  <meta charset=utf-8>
+  <title></title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Hind" rel="stylesheet">
+</head>
+<body>
+  <div class="mainWrapper">
+    <h1 class="subPageTitle">Stem van Leeuwarden</h1>
+    <h3 id="endTitle">Bedankt voor het invullen van de enquête!</h3>
+    <article class="articleEP">
+      <p>Bedankt voor het deelnemen en invullen van de enquête van De smaak van Leeuwarden. De informatie die verkregen wordt door deze enquête zal gebruikt worden voor een web applicatie die gemaakt wordt door Studenten van de NHL Leeuwrden. Voor meer informatie klik <a href="#">hier</a> voor de algemene informatie.</p>
+      <p>Bedankt!</p>
+      <!-- teksten zijn onder voorbehoud -->
+    </article>
+  </div>
+</body>
+</html>
--- preface.php
+++ preface.php
@@ -13,7 +13,6 @@
 
     <article id="prefaceP">
       Beste gebruiker. Je staat op het punt deze enquête in te vullen voor de smaak van leeuwarden. Een lopend project binnen de NHL. Door op de knop "Start Enquête" te drukken begin je met de enquête. Er zullen kort een paar vragen gesteld worden over je favoriete restaurants in Leeuwarden. Het invullen kost slechts enkele minuten.
-      <h5>Succes!</h5>
     </article>
     <div class="buttonCenter">
       <a href="#">
