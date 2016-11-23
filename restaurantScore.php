b180ebdc6f59825ac844db4a37849e8018518bbd
RenzeD98 <renze.d98@gmail.com>
2016-11-23 12:47:49 +0100

    range change slider

--- restaurantScore.php
+++ restaurantScore.php
@@ -21,7 +21,7 @@
         <label class="question" for="checkbox-1">Goede kwaliteit van het eten
         </label>
 
-        <input id="slider-1" type="range" min="1" max="11" step="1" value="6" disabled="disabled">
+        <input id="slider-1" type="range" min="0" max="10" step="1" value="5" disabled="disabled">
 
         <p class="sliderText neutraalT">Neutraal</p>
         <p class="sliderText goedT">Goed</p>
@@ -36,7 +36,7 @@
         <label class="question" for="checkbox-2">Goede prijs-kwaliteitverhouding
         </label>
 
-        <input id="slider-2" type="range" min="1" max="11" step="1" value="6" disabled="disabled">
+        <input id="slider-2" type="range" min="0" max="10" step="1" value="5" disabled="disabled">
         <p class="sliderText neutraalT">Neutraal</p>
         <p class="sliderText goedT">Goed</p>
         <p class="sliderText uitstekendT">Uitstekend</p>
@@ -49,7 +49,7 @@
         <label class="question" for="checkbox-3">Voldoende variatie in het menu
         </label>
 
-        <input id="slider-3" type="range" min="1" max="11" step="1" value="6" disabled="disabled">
+        <input id="slider-3" type="range" min="0" max="10" step="1" value="5" disabled="disabled">
         <p class="sliderText neutraalT">Neutraal</p>
         <p class="sliderText goedT">Goed</p>
         <p class="sliderText uitstekendT">Uitstekend</p>
@@ -62,7 +62,7 @@
         <label class="question" for="checkbox-4">Aangename sfeer in het restaurant
         </label>
 
-        <input id="slider-4" type="range" min="1" max="11" step="1" value="6" disabled="disabled">
+        <input id="slider-4" type="range" min="0" max="10" step="1" value="5" disabled="disabled">
         <p class="sliderText neutraalT">Neutraal</p>
         <p class="sliderText goedT">Goed</p>
         <p class="sliderText uitstekendT">Uitstekend</p>
@@ -75,7 +75,7 @@
         <label class="question" for="checkbox-5">Snelle/vriendelijke bediening
         </label>
 
-        <input id="slider-5" type="range" min="1" max="11" step="1" value="6" disabled="disabled">
+        <input id="slider-5" type="range" min="0" max="10" step="1" value="5" disabled="disabled">
         <p class="sliderText neutraalT">Neutraal</p>
         <p class="sliderText goedT">Goed</p>
         <p class="sliderText uitstekendT">Uitstekend</p>
@@ -95,35 +95,35 @@
         function disable(){
           if (document.getElementById('checkbox-1').checked == false) {
             document.getElementById('slider-1').setAttribute("disabled", "disabled");
-            document.getElementById('slider-1').value = "6";
+            document.getElementById('slider-1').value = "5";
             } else {
               document.getElementById('slider-1').removeAttribute('disabled');
             }
 
           if (document.getElementById('checkbox-2').checked == false) {
             document.getElementById('slider-2').setAttribute("disabled", "disabled");
-            document.getElementById('slider-2').value = "6";
+            document.getElementById('slider-2').value = "5";
             } else {
               document.getElementById('slider-2').removeAttribute('disabled');
             }
 
           if (document.getElementById('checkbox-3').checked == false) {
             document.getElementById('slider-3').setAttribute("disabled", "disabled");
-            document.getElementById('slider-3').value = "6";
+            document.getElementById('slider-3').value = "5";
             } else {
               document.getElementById('slider-3').removeAttribute('disabled');
             }
 
           if (document.getElementById('checkbox-4').checked == false) {
             document.getElementById('slider-4').setAttribute("disabled", "disabled");
-            document.getElementById('slider-4').value = "6";
+            document.getElementById('slider-4').value = "5";
             } else {
               document.getElementById('slider-4').removeAttribute('disabled');
             }
 
           if (document.getElementById('checkbox-5').checked == false) {
             document.getElementById('slider-5').setAttribute("disabled", "disabled");
-            document.getElementById('slider-5').value = "6";
+            document.getElementById('slider-5').value = "5";
             } else {
               document.getElementById('slider-5').removeAttribute('disabled');
             }
