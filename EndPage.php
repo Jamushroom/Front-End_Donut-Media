0e192a6cd57d671edc3ca2be76cf14d68430ddb9
Jamushroom <jamushmail@gmail.com>
2016-11-23 11:19:23 +0100

    Streamline styling

--- EndPage.php
+++ EndPage.php
@@ -9,10 +9,10 @@
 </head>
 <body>
   <div class="mainWrapper">
-    <h1 class="subPageTitle">Stem van Leeuwarden</h1>
+    <h1 class="subPageTitle">De Smaak van Leeuwarden</h1>
     <h3 id="endTitle">Bedankt voor het invullen van de enquête!</h3>
     <article class="articleEP">
-      <p>Bedankt voor het deelnemen en invullen van de enquête van De smaak van Leeuwarden. De informatie die verkregen wordt door deze enquête zal gebruikt worden voor een web applicatie die gemaakt wordt door Studenten van de NHL Leeuwrden. Voor meer informatie klik <a href="#">hier</a> voor de algemene informatie.</p>
+      <p>Bedankt voor het deelnemen en invullen van de enquête van <i>De Smaak van Leeuwarden</i>. De informatie die verkregen wordt door deze enquête zal gebruikt worden voor een web applicatie die gemaakt wordt door Studenten van de NHL Leeuwarden. Voor meer informatie klik <a href="#">hier</a> voor de algemene informatie.</p>
       <p>Bedankt!</p>
       <!-- teksten zijn onder voorbehoud -->
     </article>
